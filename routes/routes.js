eb8d68215087636ab6f0c02b1e3de556c030019a
Boipelo <Boipelo@users.noreply.github.com>
2024-06-11 21:37:56 +0200

    Auth layer re-configured

--- routes/routes.js
+++ routes/routes.js
@@ -12,13 +12,18 @@ Url = mongoose.model("Url");
 const checkToken = (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
   if (!token) {
-    res.status(401).send("Authentication Required.");
-    return;
+    return res.status(401).json({
+      status: 401,
+      message: "Not Authorized."
+    });
   }
-  jwt.verify(token, `${process.env.SECRET_KEY}`, (err, decoded) => {
-    if (err) {
-      res.status(403).send("Invalid token.");
-      return;
+
+  jwt.verify(token, `${process.env.SECRET_KEY}`, (error, decoded) => {
+    if (error) {
+      return res.status(403).json({
+        status: 403,
+        message: "Invalid Token."
+      });
     }
     req.user = decoded;
     next();
