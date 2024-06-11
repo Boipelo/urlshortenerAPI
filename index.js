ded8424588332d6a5394c7b013ef7a7c0b41a141
Boipelo <Boipelo@users.noreply.github.com>
2024-06-11 23:25:05 +0200

    Test for cors

--- index.js
+++ index.js
@@ -37,6 +37,28 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(cors()); // Not to be used in production without whitelist.
+
+app.use(function (req, res, next) {
+  if (
+    req.headers &&
+    req.headers.authorization &&
+    req.headers.authorization.split(" ")[0] === "Bearer"
+  ) {
+    jsonwebtoken.verify(
+      req.headers.authorization.split(" ")[1],
+      "TAbo5hOddJ8SKGtq",
+      function (err, decode) {
+        if (err) req.user = undefined;
+        req.user = decode;
+        next();
+      }
+    );
+  } else {
+    req.user = undefined;
+    next();
+  }
+});
+
 app.use("/api", routes);
 
 app.listen(port, () => {
